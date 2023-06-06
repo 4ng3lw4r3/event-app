dacbc703cc0122a7dea7044630eae95b8d744272
Angel Reyna <rugile@protonmail.com>
2023-06-06 13:17:08 +0300

    njumz

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -20,61 +20,90 @@ import {
   Menu,
   Close,
 } from "@mui/icons-material";
-import CategoryIcon from '@mui/icons-material/Category';
+import CategoryIcon from "@mui/icons-material/Category";
 import { useDispatch, useSelector } from "react-redux";
 // import { setMode, setLogout } from "state";
 import { useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
 import axios from "redaxios";
-import Tag from "../components/TagButton"
+import Tag from "../components/TagButton";
 // import SearchedEvent from "../components/SearchedEvent";
 
 // const SearchBar = ({ products, category, theater, setResults, onStateChange }) => {
-  function SearchBar({ onStateChange }) {
+function SearchBar({ onStateChange }) {
   const theme = useTheme();
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
-  const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const user = useSelector((state) => state.user);
   const [searchTerm, setSearchTerm] = useState("");
   const inputRef = useRef();
 
-  
-    const handleChange = (event) => {
-      const value = event.target.value;
-      setSearchTerm(value);
-    };
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+  };
 
-    const onClick = (searchTerm) => {
-      console.log(searchTerm)
-      onStateChange(searchTerm !== '' ? searchTerm : '')
-    };
-  
+  const onClick = (searchTerm) => {
+    console.log(searchTerm);
+    onStateChange(searchTerm !== "" ? searchTerm : "");
+  };
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
       {/* DESKTOP NAV */}
       {isNonMobileScreens ? (
         <FlexBetween gap="2rem">
-            {theme.palette.mode === "dark" ? (
-              <DarkMode sx={{ fontSize: "25px" }} />
-            ) : (
-              <LightMode sx={{ color: dark, fontSize: "25px" }} />
-            )}
-          <IconButton onClick={() => onClick('Fantasy life')} sx={{ fontSize: "20px" }}> Fantasy Life</IconButton>
-          <IconButton onClick={() => onClick('New age')} sx={{ fontSize: "20px" }}> New Age</IconButton>
-          <IconButton onClick={() => onClick('For animals')} sx={{ fontSize: "20px" }}> For Animals</IconButton>
-          <IconButton onClick={() => onClick('Concert')} sx={{ fontSize: "20px" }}> Concert</IconButton>
-          <IconButton onClick={() => onClick('Theater')} sx={{ fontSize: "20px" }}> Theater</IconButton>
-          <IconButton onClick={() => onClick('Rave')} sx={{ fontSize: "20px" }}> Rave</IconButton>
-          <IconButton onClick={() => onClick('Club')} sx={{ fontSize: "20px" }}> Club</IconButton>
+          {theme.palette.mode === "dark" ? (
+            <DarkMode sx={{ fontSize: "25px" }} />
+          ) : (
+            <LightMode sx={{ color: dark, fontSize: "25px" }} />
+          )}
+          <IconButton
+            onClick={() => onClick("Fantasy life")}
+            sx={{ fontSize: "20px" }}
+          >
+            {" "}
+            Fantasy Life
+          </IconButton>
+          <IconButton
+            onClick={() => onClick("New age")}
+            sx={{ fontSize: "20px" }}
+          >
+            {" "}
+            New Age
+          </IconButton>
+          <IconButton
+            onClick={() => onClick("For animals")}
+            sx={{ fontSize: "20px" }}
+          >
+            {" "}
+            For Animals
+          </IconButton>
+          <IconButton
+            onClick={() => onClick("Concert")}
+            sx={{ fontSize: "20px" }}
+          >
+            {" "}
+            Concert
+          </IconButton>
+          <IconButton
+            onClick={() => onClick("Theater")}
+            sx={{ fontSize: "20px" }}
+          >
+            {" "}
+            Theater
+          </IconButton>
+          <IconButton onClick={() => onClick("Rave")} sx={{ fontSize: "20px" }}>
+            {" "}
+            Rave
+          </IconButton>
+          <IconButton onClick={() => onClick("Club")} sx={{ fontSize: "20px" }}>
+            {" "}
+            Club
+          </IconButton>
         </FlexBetween>
       ) : (
         <IconButton
@@ -99,9 +128,7 @@ import Tag from "../components/TagButton"
             // onChange={(e) => setSearchTerm(e.target.value)}
             onChange={handleChange}
           />
-          <IconButton 
-          onClick={() => onClick(searchTerm)}
-          >
+          <IconButton onClick={() => onClick(searchTerm)}>
             <Search />
           </IconButton>
         </FlexBetween>
@@ -136,20 +163,60 @@ import Tag from "../components/TagButton"
             alignItems="center"
             gap="3rem"
           >
-          <IconButton onClick={() => onClick('Fantasy life')} sx={{ fontSize: "20px" }}> Fantasy Life</IconButton>
-          <IconButton onClick={() => onClick('New age')} sx={{ fontSize: "20px" }}> New Age</IconButton>
-          <IconButton onClick={() => onClick('For animals')} sx={{ fontSize: "20px" }}> For Animals</IconButton>
-          <IconButton onClick={() => onClick('Concert')} sx={{ fontSize: "20px" }}> Concert</IconButton>
-          <IconButton onClick={() => onClick('Theater')} sx={{ fontSize: "20px" }}> Theater</IconButton>
-          <IconButton onClick={() => onClick('Rave')} sx={{ fontSize: "20px" }}> Rave</IconButton>
-          <IconButton onClick={() => onClick('Club')} sx={{ fontSize: "20px" }}> Club</IconButton>
+            <IconButton
+              onClick={() => onClick("Fantasy life")}
+              sx={{ fontSize: "20px" }}
+            >
+              {" "}
+              Fantasy Life
+            </IconButton>
+            <IconButton
+              onClick={() => onClick("New age")}
+              sx={{ fontSize: "20px" }}
+            >
+              {" "}
+              New Age
+            </IconButton>
+            <IconButton
+              onClick={() => onClick("For animals")}
+              sx={{ fontSize: "20px" }}
+            >
+              {" "}
+              For Animals
+            </IconButton>
+            <IconButton
+              onClick={() => onClick("Concert")}
+              sx={{ fontSize: "20px" }}
+            >
+              {" "}
+              Concert
+            </IconButton>
+            <IconButton
+              onClick={() => onClick("Theater")}
+              sx={{ fontSize: "20px" }}
+            >
+              {" "}
+              Theater
+            </IconButton>
+            <IconButton
+              onClick={() => onClick("Rave")}
+              sx={{ fontSize: "20px" }}
+            >
+              {" "}
+              Rave
+            </IconButton>
+            <IconButton
+              onClick={() => onClick("Club")}
+              sx={{ fontSize: "20px" }}
+            >
+              {" "}
+              Club
+            </IconButton>
           </FlexBetween>
-
-          
         </Box>
       )}
     </FlexBetween>
   );
-};
+}
 
 export default SearchBar;
