e3f2962a1112b99c0c68d4c76f067162905944a7
Angel Reyna <rugile@protonmail.com>
2023-06-23 11:45:27 +0300

    exam

---
+++ client/src/components/UserImage.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material"
+import hellookitty from "../assets/hellookitty.jpg"
+
+const UserImage = ({ size }) => {
+  return (
+    <Box
+     width={size} height={size}
+     >
+        <img
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        width="80rem"
+        height="80rem"
+        alt="user"
+        src={hellookitty}        
+        />
+    </Box>
+    )
+}
+
+export default UserImage
